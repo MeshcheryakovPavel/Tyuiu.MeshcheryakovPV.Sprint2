0fb29ff9c19186951497a3bcb1929f996b3ec9d0
Tyuiu.MeshcheryakovPV@outlook.com <pavellol202@gmail.com>
2023-10-05 19:52:11 +0300

    Task2

---
+++ Tyuiu.MeshcheryakovPV.Sprint2.Task2.V30.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MeshcheryakovPV.Sprint2.Task2.V30.Lib;
+
+namespace Tyuiu.MeshcheryakovPV.Sprint2.Task2.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 4;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
