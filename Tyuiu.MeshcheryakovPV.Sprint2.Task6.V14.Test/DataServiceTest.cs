7e70a886774634788d8a6030264a04a5a4560d6a
Tyuiu.MeshcheryakovPV@outlook.com <pavellol202@gmail.com>
2023-10-14 18:05:37 +0300

    Task6

---
+++ Tyuiu.MeshcheryakovPV.Sprint2.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MeshcheryakovPV.Sprint2.Task6.V14.Lib;
+
+namespace Tyuiu.MeshcheryakovPV.Sprint2.Task6.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDayName()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("Понедельник", ds.FindDayName(1, 1));
+            Assert.AreEqual("Вторник", ds.FindDayName(2, 1));
+            Assert.AreEqual("Среда", ds.FindDayName(3, 1));
+            Assert.AreEqual("Четверг", ds.FindDayName(4, 1));
+            Assert.AreEqual("Пятница", ds.FindDayName(5, 1));
+            Assert.AreEqual("Суббота", ds.FindDayName(6, 1));
+            Assert.AreEqual("Воскресенье", ds.FindDayName(7, 1));
+
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindDayName(-1, -1);
+
+            });
+
+        }
+    }
+}
---
+++ Tyuiu.MeshcheryakovPV.Sprint2.Task6.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MeshcheryakovPV.Sprint2.Task6.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MeshcheryakovPV.Sprint2.Task6.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db75bbff-fcc2-446c-939d-26ba5b09b65f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
