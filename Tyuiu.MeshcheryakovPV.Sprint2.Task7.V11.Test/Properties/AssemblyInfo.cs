641fd0effbc80f723a707274b6aadce3afc5e57f
Tyuiu.MeshcheryakovPV@outlook.com <pavellol202@gmail.com>
2023-10-15 21:57:00 +0300

    Task7

---
+++ Tyuiu.MeshcheryakovPV.Sprint2.Task7.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MeshcheryakovPV.Sprint2.Task7.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MeshcheryakovPV.Sprint2.Task7.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8381f8b-2553-4bef-b75d-0568bc820e75")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
